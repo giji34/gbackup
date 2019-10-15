9f777b320db6da443ab36ed318455f5fe8ea19b0
kbinani <kbinani.bt@gmail.com>
2019-10-15 17:29:09 +0900

    1: 20191015 1726

--- backup.sh
+++ backup.sh
@@ -2,12 +2,7 @@
 
 last_message=$(git log --pretty=format:"%s" | head -1)
 last_num_players=$(echo "$last_message" | cut -d: -f1 | bc)
-
-./active_players
-
 num_players=$(./active_players 2>/dev/null)
-echo num_players=$num_players
-echo last_num_players=$last_num_players
 
 if [ "$num_players" -gt 0 -o "$num_players" -ne "$last_num_players" ]; then
 	commit_msg="$num_players: $(date "+%Y%m%d %H%M")"
--- git-add.sh
+++ git-add.sh
@@ -2,6 +2,23 @@
 
 (
 	cd "$(dirname "$0")"
+	(
+		cd world
+		mkdir -p chunk
+		cd chunk
+		../../split-regions-to-chunks ..
+	)
+	(
+		cd world_nether/DIM-1
+		mkdir -p chunk
+		cd chunk
+		../../../split-regions-to-chunks ..
+	)
+	(
+		cd world_the_end/DIM1
+		mkdir -p chunk
+		cd chunk
+		../../../split-regions-to-chunks ..
+	)
 	git add -A world world_nether world_the_end
 )
-
