b4f508eefaa3a1acf991a64d63c50387a499dd68
kbinani <kbinani.bt@gmail.com>
2019-10-18 01:18:59 +0900

    split-regions-to-chunks にファイルのパスも渡せるようにする

--- src/split-regions-to-chunks/main.cpp
+++ src/split-regions-to-chunks/main.cpp
@@ -5,26 +5,41 @@ using namespace std;
 using namespace mcfile;
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("split-regions-to-chunks: split *.mca to per-chunk, compressed files to current directory\n");
         printf("Usage:\n");
-        printf("    split-regions-to-chunks [world directory containing 'region' directory]\n");
+        printf("    split-regions-to-chunks [mca file or world directory containing 'region' directory]\n");
         printf("Decompress to nbt:\n");
         printf("    pigz -z -d < c.0.0.nbt.z > c.0.0.nbt\n");
         return 1;
     }
 
-    std::string worldDir = argv[1];    
-    World w(worldDir);
+    namespace fs = std::filesystem;
 
     hwm::task_queue q(thread::hardware_concurrency());
     vector<future<void>> futures;
 
-    w.eachRegions([&q, &futures](auto const& region) {
-        futures.emplace_back(q.enqueue([](auto const& region) {
-            region->exportAllToCompressedNbt("./");
-        }, region));
-    });
+    for (int i = 1; i < argc; i++) {
+        string item = argv[i];
+        fs::path p = item;
+        error_code ec;
+        if (fs::is_directory(p, ec)) {
+            World w(item);
+            w.eachRegions([&q, &futures](shared_ptr<Region> const& region) {
+                futures.emplace_back(q.enqueue([](shared_ptr<Region> const& region) {
+                    region->exportAllToCompressedNbt("./");
+                }, region));
+            });
+        } else if (fs::is_regular_file(p, ec)) {
+            auto region = Region::MakeRegion(item);
+            if (!region) {
+                continue;
+            }
+            futures.emplace_back(q.enqueue([](auto const& region) {
+                region->exportAllToCompressedNbt("./");
+            }, region));
+        }
+    }
 
     for (auto& f : futures) {
         f.get();
