e993b7660fc57b4b1cd1c58b336a2942a9ee93da
kbinani <kbinani.bt@gmail.com>
2019-10-18 01:20:14 +0900

    リージョンファイルのチェックサムを保存しておき変更されたファイルだけチャンクに分解する

--- git-add.sh
+++ git-add.sh
@@ -2,23 +2,27 @@
 
 (
 	cd "$(dirname "$0")"
-	(
-		cd world
-		mkdir -p chunk
-		cd chunk
-		../../split-regions-to-chunks ..
-	)
-	(
-		cd world_nether/DIM-1
-		mkdir -p chunk
-		cd chunk
-		../../../split-regions-to-chunks ..
-	)
-	(
-		cd world_the_end/DIM1
-		mkdir -p chunk
-		cd chunk
-		../../../split-regions-to-chunks ..
-	)
+	split_regions_to_chunks="$(pwd)/split-regions-to-chunks"
+	checksum="$(pwd)/checksum.sh"
+
+	tmp1=$(mktemp)
+	tmp2=$(mktemp)
+
+	for w in world world_nether/DIM-1 world_the_end/DIM1; do
+		(
+			cd "$w/region"
+			bash "$checksum" > "$tmp1"
+		)
+		(
+			cd $w
+			mkdir -p chunk
+			cd chunk
+			(cat region_checksum.txt; cat "$tmp1") | sort | uniq -c | awk '{print $1, $2, $3}' | grep -v '^2' | awk '{print $2}' | sort | uniq | sed 's:^\(.*\)$:../region/\1:g' > "$tmp2"
+			cat "$tmp2" | xargs "$split_regions_to_chunks"
+			cp "$tmp1" ./region_checksum.txt
+			git add region_checksum.txt
+		)
+	done
 	git add -A world world_nether world_the_end
+	rm -f "$tmp1" "$tmp2"
 )
