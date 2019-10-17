2114bd928d9f4c256c1c202d10743e70247a7d23
kbinani <kbinani.bt@gmail.com>
2019-10-18 01:51:38 +0900

    sha1sum の代わりに openssl sha1 -r を使う

--- checksum.sh
+++ checksum.sh
@@ -6,4 +6,4 @@ file_per_proc=$(echo "$num_files / $nproc" | bc)
 if [ $file_per_proc -lt 1 ]; then
 	file_per_proc=1
 fi
-(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | xargs -P $nproc -n $file_per_proc sha1sum) | awk '{print $2, $1}' | sort
+(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | xargs -P $nproc -n $file_per_proc openssl sha1 -r) | tr -d '*' | awk '{print $2, $1}' | sort
