203296a83558e0674123c4599968ecd759eb0e69
kbinani <kbinani.bt@gmail.com>
2019-10-29 16:49:16 +0000

    checksum.sh が Linux でも動くようにする

--- checksum.sh
+++ checksum.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-nproc=$(getconf _NPROCESSORS_ONLN)
-num_files=$(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | wc -l | bc)
-file_per_proc=$(echo "$num_files / $nproc" | bc)
-if [ $file_per_proc -lt 1 ]; then
-	file_per_proc=1
+if type nproc 2>&1 >/dev/null; then
+  NPROC=$(nproc)
+else
+  NPROC=$(getconf _NPROCESSORS_ONLN)
 fi
-(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | xargs -P $nproc -n $file_per_proc openssl sha1 -r) | tr -d '*' | awk '{print $2, $1}' | sort
+
+num_files=$(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | wc -l | bc)
+file_per_proc=$(echo "$num_files / $NPROC" | bc)
+if [ $file_per_proc -lt 1 ]; then
+  file_per_proc=1
+fi
+(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | xargs -P $NPROC -n $file_per_proc openssl sha1 -r) | tr -d '*' | awk '{print $2, $1}' | sort
