c285a817c5ddffbe415bf029c789c321cb5ae335
kbinani <kbinani.bt@gmail.com>
2020-01-29 03:20:23 +0900

    fix: region_checksum.txt ファイルが壊れる問題を修正

--- checksum.sh
+++ checksum.sh
@@ -11,4 +11,6 @@ file_per_proc=$(echo "$num_files / $NPROC" | bc)
 if [ $file_per_proc -lt 1 ]; then
   file_per_proc=1
 fi
-(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | xargs -P $NPROC -n $file_per_proc openssl sha1 -r) | tr -d '*' | awk '{print $2, $1}' | sort
+work=$(mktemp -d)
+cat $(ls -1 | grep '^r\.[0-9-]*\.[0-9-]*\.mca$' | parallel --no-notice --files --tmpdir "$work" -P $NPROC -n $file_per_proc openssl sha1 -r) | tr -d '*' | awk '{print $2, $1}'
+rm -rf "$work"
