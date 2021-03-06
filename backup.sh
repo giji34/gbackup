cac132f610f7f59c5a69ee1fe76caaf12fa134d6
kbinani <kbinani.bt@gmail.com>
2021-03-06 09:37:26 +0900

    feat: backup-migrate.sh を撤去

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 set -ue
 
 mcdir="$1"
-gitdir="$2"
+hgdir="$2"
 queryport="$3"
 tooldir="$(cd "$(dirname "$0")"; pwd)"
 
@@ -17,7 +17,7 @@ lockfile="$tooldir/lock.pid"
 		exit
 	}
 
-	last_message=$(cd "$gitdir" && git log --pretty=format:"%s" | grep '^[0-9]*: [0-9]\{8\} [0-9]\{4\}$' | head -1)
+	last_message=$(cd "$hgdir" && hg log --template '{desc}\n' | grep '^[0-9]*: [0-9]\{8\} [0-9]\{4\}$' | head -1)
 	last_num_players=$(echo "$last_message" | cut -d: -f1 | bc)
 	num_players=$(${tooldir}/active_players ${queryport} 2>/dev/null || echo 0)
 
@@ -39,23 +39,23 @@ lockfile="$tooldir/lock.pid"
 			fi
 		)
 		(
-			mkdir -p "$gitdir/$w/chunk"
-			cd "$gitdir/$w/chunk"
-			(cat "$gitdir/$w/chunk/region_checksum.txt"; cat "$tmp1") | sort | uniq -u | awk "{printf \"$mcdir/$w/region/%s\n\", \$1}" | sort | uniq > "$tmp2"
+			mkdir -p "$hgdir/$w/chunk"
+			cd "$hgdir/$w/chunk"
+			(cat "$hgdir/$w/chunk/region_checksum.txt"; cat "$tmp1") | sort | uniq -u | awk "{printf \"$mcdir/$w/region/%s\n\", \$1}" | sort | uniq > "$tmp2"
 			if [ -s "$tmp2" ]; then
 				cat "$tmp2" | xargs "$split_regions_to_chunks"
 				cp "$tmp1" ./region_checksum.txt
 			fi
 		)
-		mkdir -p "$gitdir/$w/data"
 		if [ -d "$mcdir/$w/data" ]; then
-			rsync -av --delete "$mcdir/$w/data/" "$gitdir/$w/data/"
+			mkdir -p "$hgdir/$w/data"
+			rsync -av --delete "$mcdir/$w/data/" "$hgdir/$w/data/"
 		fi
 	done
 	(
-		cd "$gitdir"
-		git add -A world world_nether world_the_end
-		git commit -m "$commit_msg"
+		cd "$hgdir"
+		hg addremove world world_nether world_the_end
+		hg commit -m "$commit_msg"
 	)
 	rm -f "$tmp1" "$tmp2"
 ) 200> "$lockfile"
